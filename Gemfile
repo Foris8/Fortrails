c2640205926aff9481d2737505e94ce32f0a1810
Foris8 <foristang@gmail.com>
2023-09-14 17:34:07 -0400

    Finish Backend User Auth and seed some user info

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem "bootsnap", require: false
 # gem "rack-cors"
 gem "jbuilder"
 gem "bcrypt"
+gem "faker"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ app/models/user.rb
@@ -0,0 +1,42 @@
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, 
+        uniqueness: true, 
+        length: { in: 6..255 }, 
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { in: 6..255 }, allow_nil: true
+    
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        # has_secure_password gives us the authenticate method
+        if user&.authenticate(password) 
+            return user
+        else
+            nil 
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        save!
+        session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        while true
+            token = SecureRandom.urlsafe_base64
+            return token unless User.exists?(session_token: token)
+        end
+    end
+
+end
