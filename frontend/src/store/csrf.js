be2cf55b1fb54be4456525f59539b310514fedba
Foris8 <foristang@gmail.com>
2023-09-15 09:30:47 -0400

    start frontend user auth

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,8 @@
+
+export const restoreSession = async () => {
+    let res = await fetch('/api/session');
+    let token = res.headers.get('X-CSRF-Token');
+    sessionStorage.setItem('X-CSRF-Token', token);
+    let data = await res.json();
+    sessionStorage.setItem('currentUser', JSON.stringify(data.user));
+}
